2e770ec1aa54366a6aa5f25e2ab1e9dad98582f1
woliaoa <a997927885@gmail.com>
2020-09-07 17:02:11 +0800

    在家继续开发签到功能

---
+++ SignIn.java
@@ -0,0 +1,10 @@
+public class Sign{
+	private int id;
+	private String age;
+	public void sign(){
+		System.out.println("在家开发的代码");
+	}
+	public void sign(int id){
+		System.out.println("在家开发的代码");
+	}
+}
